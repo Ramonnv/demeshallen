b00ed2ee5265f0e83802b8ae19ccb82fe3f6fd0c
Collin-FFG <c.hamer@fairfurnituregroup.com>
2025-06-30 16:58:40 +0200

    Added php function for loading svg's on styling page

--- web/templates/style-testpage.php
+++ web/templates/style-testpage.php
@@ -10,18 +10,6 @@ function svg() {
     return '<svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512"><path d="M64 112c-8.8 0-16 7.2-16 16l0 22.1L220.5 291.7c20.7 17 50.4 17 71.1 0L464 150.1l0-22.1c0-8.8-7.2-16-16-16L64 112zM48 212.2L48 384c0 8.8 7.2 16 16 16l384 0c8.8 0 16-7.2 16-16l0-171.8L322 328.8c-38.4 31.5-93.7 31.5-132 0L48 212.2zM0 128C0 92.7 28.7 64 64 64l384 0c35.3 0 64 28.7 64 64l0 256c0 35.3-28.7 64-64 64L64 448c-35.3 0-64-28.7-64-64L0 128z"/></svg>';
 }
 
-function get_svg($filename, $directory = 'icons') {
-    $filepath = __DIR__ . '/images/' . $directory . '/' . $filename . '.svg';
-
-    if (file_exists($filepath)) {
-        return file_get_contents($filepath);
-    } else {
-        error_log("SVG not found: $filepath");
-        return '';
-    }
-}
-
-
 
 $hasContent = !empty( get_the_content() );
 
@@ -72,15 +60,14 @@ $hasContent = !empty( get_the_content() );
                 <h1>All used SVG's</h1>
                 <?php 
                 
-                    $directory = __DIR__ . '/images/icons';
+                    $directory = get_template_directory() . '/src/images/icons/';
                     $svgFiles = glob($directory . '/*.svg');
-
+                    
                     foreach ($svgFiles as $filePath) {
                         $filename = pathinfo($filePath, PATHINFO_FILENAME);
-                        echo get_svg($filename, 'icons');
+                        $x = get_template_directory_uri() . '/src/images/icons/' . $filename;
+                        echo '<img width="80" height="80" src="'.$x.'.svg" alt="'.$filename.'">';
                     }
-                    var_dump(get_svg('at'))
-
 
                 ?>
             </div>
