d528cfae4677cdce525232241ae0d0a297ab87e7
Collin Hammer <c.hamer@fairfurnituregroup.com>
2025-08-08 11:51:32 +0200

    Added contact and meeting popup styling...

--- web/functions.php
+++ web/functions.php
@@ -41,38 +41,6 @@ function run_fairbase_starter_theme() {
 
 
 
-add_filter('gform_field_content', 'custom_gform_field_wrapper_by_id', 10, 5);
-
-function custom_gform_field_wrapper_by_id($field_content, $field, $value, $lead_id, $form_id) {
-    // Define custom wrapper classes for specific field IDs
-    $gravity_settings = array(
-        1 => 'Test',
-        3 => 'class_two',
-        5 => 'class_three',
-    );
-
-    // Check if this field ID has a custom class
-    if (array_key_exists($field->id, $gravity_settings)) {
-        $custom_svg = $gravity_settings[$field->id];
-
-        // Wrap the original field content with a div with the custom class
-        $field_content = sprintf(
-            '
-				<div class="custom-thingie">
-                            %s
-                            %s
-                        </div>
-
-			',
-            $custom_svg,
-            $field_content
-        );
-    }
-
-    return $field_content;
-}
-
-
 
 
 
